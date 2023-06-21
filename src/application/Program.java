3f4d8793af927bd2ac3b34d96f97c876cb6f0330
Jonathan Muniz <Jjoninha11@hotmail.com>
2023-06-21 12:05:40 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
+		// testes de implementações novas
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+// DAO funciona basicamente igual um Repository
+public interface DepartmentDao {
+
+	void insert(Department obj);
+
+	void update(Department obj);
+
+	void deleteById(Integer id);
+
+	Department findById(Integer id);
+
+	List<Department> findAll();
+
+}
